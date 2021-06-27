fbdfa54fd68eefe13f1142c38eab82601b0a7e00
Mukesh kumar singh <mukesh.19je0540@ee.iitism.ac.in>
2021-06-28 01:24:04 +0530

    improved ui

--- public/script.js
+++ public/script.js
@@ -5,6 +5,7 @@ const videoGrid = document.getElementById('video-grid')
 
 //adding name in chatbox//
 const username = prompt("Please enter your name", "<name goes here>");
+
 //console.log(username)
 $('.messages').append(`<div class="messages_center">you joined<br></div>`);
 socket.emit('new-user', username);
@@ -14,16 +15,31 @@ socket.emit('new-user', username);
 const peer = new Peer(undefined)
 let myVideoStream;
 const myVideo = document.createElement('video')
+//const name= document.createElement('name');
+
+
 myVideo.muted = false;
 const peers = {}
+
+//const constraintsVideo = {
+//  audio:false,
+//  video:true,
+//}
+//const constraintsAudio = {
+////  audio:true,
+//}
 navigator.mediaDevices.getUserMedia({
   video: true,
-  audio: true
+  audio: 
+ {
+    echoCancellation: true,
+    noiseSuppression:true,
+  }
+  //constraintsVideo,
+  //constraintsAudio
 }).then(stream => {
   myVideoStream = stream;
   addVideoStream(myVideo, stream)
-
-
   peer.on('call', call => {
     console.log('answering call');
     call.answer(stream)
@@ -44,25 +60,25 @@ navigator.mediaDevices.getUserMedia({
 
 
   socket.on('user-joined', (username) => {
-   // console.log("user-name: " + username);
+    console.log("user-name: " + username);
     $('.messages').append(`<div class="messages_center"><b>${username}</b> joined the chat<br></div>`);
   }) 
 
- 
+  peer.on('open', id => {
+    console.log(id);
+    socket.emit('join-room', ROOM_ID, id)
+  })
+  
+  socket.on('chat-message',data =>{
+    console.log(data.name);
+    $('.messages').append(`<div class="messages_left"><b>${data.name}</b>:${data.message}</div>`);
+  })
+  
 
 
 })
 
 
-peer.on('open', id => {
-  console.log(id);
-  socket.emit('join-room', ROOM_ID, id)
-})
-
-socket.on('chat-message',data =>{
-  console.log(data.name);
-  $('.messages').append(`<div class="messages_left"><b>${data.name}</b>:${data.message}</div>`);
-})
 
 
 
@@ -98,35 +114,37 @@ const connectToNewUser= (userID,stream) => {
 
 
 const addVideoStream = (video, stream) =>{
-    console.log('hey bro');
+    console.log('my video appended');
     video.srcObject = stream
     video.addEventListener('loadedmetadata', () => {
       video.play()
     })
     videoGrid.append(video)
+ 
   }
  
   const sendMessage=(e) =>{
+    let msgtext=$('input');
+    console.log(msgtext);
+   if( msgtext.val().length!=0){
+  
+    console.log('i m inside sendmesaage fun') 
+    $('.messages').append(`<div class="messages_right"><b>Me:</b> ${msgtext.val()}</div>`);
+    socket.emit('message',msgtext.val());
    
-    let text=$('input'); 
-   if( text.val().length!=0){
-    console.log(text.val());
-    $('.messages').append(`<div class="messages_right"><b>Me:</b> ${text.val()}</div>`);
-    socket.emit('message',text.val());
-   
-    text.val('');
+    msgtext.val('');
   }
   }
 
 
-  let text=$('input'); 
+  let msgtext=$('input'); 
   $('html').keydown((e) =>{
-    if(e.which==13 && text.val().length!=0){
-      console.log(text.val());
-      $('.messages').append(`<div class="messages_right"><b>Me:<t></b> ${text.val()}</div>`);
-      socket.emit('message',text.val());
+    if(e.which==13 && msgtext.val().length!=0){
+      console.log(msgtext);
+      $('.messages').append(`<div class="messages_right"><b>Me:<t></b> ${msgtext.val()}</div>`);
+      socket.emit('message',msgtext.val());
      
-      text.val('');
+      msgtext.val('');
     }
 
   })
@@ -174,6 +192,7 @@ const playStop = () => {
   let enabled = myVideoStream.getVideoTracks()[0].enabled;
   if (enabled) {
     myVideoStream.getVideoTracks()[0].enabled = false;
+    
     setPlayVideo()
   } else {
     setStopVideo()
@@ -187,6 +206,7 @@ const setStopVideo = () => {
     <span>Stop Video</span>
   `
   document.querySelector('.main__video_button').innerHTML = html;
+
 }
 
 const setPlayVideo = () => {
@@ -198,3 +218,303 @@ const setPlayVideo = () => {
 }
 
 
+
+
+ /*  <--Screen Sharing Code --> */
+  
+  function handleSuccess(stream) {
+    startButton.disabled = true;      
+    const video = document.querySelector('video');
+    video.srcObject = stream;
+  
+    // demonstrates how to detect that the user has stopped
+    // sharing the screen via the browser UI.
+    stream.getVideoTracks()[0].addEventListener('ended', () => {
+      errorMsg('The user has ended sharing the screen');
+      startButton.disabled = false;
+    });
+  }
+  
+  
+ const shareScreen = () => {
+    console.log('i m under share screen funtion');
+  const startButton = document.getElementById('startButton');
+  startButton.addEventListener('click', () => {
+    navigator.mediaDevices.getDisplayMedia({video: true})
+        .then(handleSuccess);
+  });
+}
+
+  
+  if ((navigator.mediaDevices && 'getDisplayMedia' in navigator.mediaDevices)) {
+    startButton.disabled = false;
+  } else {
+    errorMsg('getDisplayMedia is not supported');
+  }
+
+  /*  <--Screen Sharing Code ends --> */
+
+
+
+  /* <--Screen Recording,Play and Download Code starts--> */
+
+
+let mediaRecorder;
+let recordedBlobs;
+
+const codecPreferences = document.querySelector('#codecPreferences');
+
+//const errorMsgElement = document.querySelector('span#errorMsg');
+const recordedVideo = document.querySelector('video#recorded');
+
+
+
+
+const playButton = document.querySelector('button#play');
+playButton.addEventListener('click', () => {
+  const mimeType = codecPreferences.options[codecPreferences.selectedIndex].value.split(';', 1)[0];
+  const superBuffer = new Blob(recordedBlobs, {type: mimeType});
+  recordedVideo.src = null;
+  recordedVideo.srcObject = null;
+
+  recordedVideo.src = window.URL.createObjectURL(superBuffer);
+  recordedVideo.controls = true;
+  recordedVideo.play();
+});
+
+const downloadButton = document.querySelector('button#download');
+downloadButton.addEventListener('click', () => {
+  const blob = new Blob(recordedBlobs, {type: 'video/webm'});
+  const url = window.URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.style.display = 'none';
+  a.href = url;
+  a.download = 'test.webm';
+  document.body.appendChild(a);
+  a.click();
+  setTimeout(() => {
+    document.body.removeChild(a);
+    window.URL.revokeObjectURL(url);
+  }, 100);
+});
+
+function handleDataAvailable(event) {
+  console.log('handleDataAvailable', event);
+  if (event.data && event.data.size > 0) {
+    recordedBlobs.push(event.data);
+  }
+}
+
+function getSupportedMimeTypes() {
+ const possibleTypes = [
+    'video/webm;codecs=vp9,opus',
+   'video/webm;codecs=vp8,opus',
+   'video/webm;codecs=h264,opus',
+    'video/mp4;codecs=h264,aac',
+  ];
+  return possibleTypes.filter(mimeType => {
+    return MediaRecorder.isTypeSupported(mimeType);
+  });
+}
+
+function startRecording() {
+  recordedBlobs = [];
+  const mimeType = codecPreferences.options[codecPreferences.selectedIndex].value;
+  const options = {mimeType};
+
+  try {
+    mediaRecorder = new MediaRecorder(window.stream, options);
+  } catch (e) {
+    console.error('Exception while creating MediaRecorder:', e);
+    errorMsgElement.innerHTML = `Exception while creating MediaRecorder: ${JSON.stringify(e)}`;
+    return;
+  }
+
+  console.log('Created MediaRecorder', mediaRecorder, 'with options', options);
+  recordButton.textContent = 'Stop Recording';
+  playButton.disabled = true;
+  downloadButton.disabled = true;
+  codecPreferences.disabled = true;
+  mediaRecorder.onstop = (event) => {
+    console.log('Recorder stopped: ', event);
+    console.log('Recorded Blobs: ', recordedBlobs);
+  };
+  mediaRecorder.ondataavailable = handleDataAvailable;
+  mediaRecorder.start();
+  console.log('MediaRecorder started', mediaRecorder);
+}
+
+function stopRecording() {
+  mediaRecorder.stop();
+}
+
+function handleSuccess(stream) {
+  recordButton.disabled = false;
+  console.log('getUserMedia() got stream:', stream);
+  window.stream = stream;
+
+  const gumVideo = document.querySelector('video');
+  gumVideo.srcObject = stream;
+
+  getSupportedMimeTypes().forEach(mimeType => {
+    const option = document.createElement('option');
+    option.value = mimeType;
+    option.innerText = option.value;
+    codecPreferences.appendChild(option);
+  });
+  codecPreferences.disabled = false;
+}
+
+async function init(constraints) {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    handleSuccess(stream);
+  } catch (e) {
+    console.error('navigator.getUserMedia error:', e);
+    errorMsgElement.innerHTML = `navigator.getUserMedia error:${e.toString()}`;
+  }
+}
+
+
+ 
+
+const recordButton = document.querySelector('button#record');
+recordButton.addEventListener('click', () => {
+  if (recordButton.textContent === 'Start Recording') {
+    startRecording();
+  } else {
+    stopRecording();
+    recordButton.textContent = 'Start Recording';
+    playButton.disabled = false;
+    downloadButton.disabled = false;
+    codecPreferences.disabled = false;
+  }
+});
+
+
+function startRecord(){
+document.querySelector('button#start').addEventListener('click', async () => {
+  document.querySelector('button#start').disabled = true;
+  const hasEchoCancellation = document.querySelector('#echoCancellation').checked;
+  const constraints = {
+    audio: {
+      echoCancellation: {exact: hasEchoCancellation}
+    },
+    video: {
+      width: 1280, height: 720
+    }
+  };
+  console.log('Using media constraints:', constraints);
+  await init(constraints);
+});
+}
+
+
+  /* <--Screen Recording,Play and Download Code ends--> */
+
+  
+
+
+
+  /* WhiteBoard Code Begins */
+  const whitetable = () =>{
+
+    /* Initialise variables */
+    let isDrawing = false;
+    let x = 0;
+    let y = 0;
+    
+let eraseEnable = false;
+
+    /* Get canvas and context */
+    const canvas = document.getElementById('sheet');
+    var context = canvas.getContext('2d');
+
+    /* Add the event listeners for mousedown, mousemove, and mouseup */
+    canvas.addEventListener('mousedown', e => {
+        /* Drawing begins */
+        x = e.offsetX;
+        y = e.offsetY;
+        isDrawing = true;
+    });
+    
+    canvas.addEventListener('mousemove', e => {
+        /* Drawing continues */
+        if (isDrawing === true) {
+            drawLine(context, x, y, e.offsetX, e.offsetY);
+            x = e.offsetX;
+            y = e.offsetY;
+        }
+    });
+    
+    window.addEventListener('mouseup', e => {
+        /* Drawing ends */
+        if (isDrawing === true) {
+            drawLine(context, x, y, e.offsetX, e.offsetY);
+            x = 0;
+            y = 0;
+            isDrawing = false;
+        }
+    });
+
+    /* Initialise socket */
+   // var socket = io();
+
+    /* Receiving Updates from server */
+    socket.on('update_canvas',function(data){
+        let {x1,y1,x2,y2,color} = JSON.parse(data);
+        drawLine(context,x1,y1,x2,y2,color,true);
+    });
+
+    /* Function to Draw line from (x1,y1) to (x2,y2) */
+    function drawLine(context, x1, y1, x2, y2,color = selected_color,from_server = false) {
+
+        /* Send updates to server (not re-emiting those received from server) */
+        if(!from_server)
+            socket.emit('update_canvas',JSON.stringify({x1,y1,x2,y2,color}));
+        
+        /* Draw line with color, stroke etc.. */
+        context.beginPath();
+        context.strokeStyle = color;
+        context.lineWidth = 5;
+        context.lineCap = 'round'
+        context.moveTo(x1, y1);
+        context.lineTo(x2, y2);
+        context.stroke();
+        context.closePath();
+        }
+
+     /* helper function to change selected_color
+   triggered onclick buttons below canvas
+   'red','green','blue'
+ */
+
+
+}
+let selected_color = 'red';
+function selectColor(color){
+    document.getElementsByClassName(selected_color)[0].classList.remove('selected');
+    document.getElementsByClassName(color)[0].classList.add('selected');    
+    selected_color = color;
+}  
+
+toggleBtn = createButton("Toggle erase");
+  toggleBtn.position(30, 60);
+  toggleBtn.mouseClicked(toggleErase);
+
+  function toggleErase() {
+    if (eraseEnable) {
+      noErase();
+      eraseEnable = false;
+    }
+    else {
+      erase();
+      eraseEnable = true;
+    }
+  }
+    
+  function mouseMoved() {
+    fill('red');
+    noStroke();
+    circle(mouseX, mouseY, 50);
+  }
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express=require('express');
 const app=express();
 const port=process.env.PORT || 8080;
+const nodemailer = require('nodemailer');
+const mailGun=require('nodemailer-mailgun-transport');
+var smtpTransport = require('nodemailer-smtp-transport');
+
 
 const server = require('http').Server(app)
 const io = require('socket.io')(server);
@@ -16,6 +20,12 @@ const users={};
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use("/peerjs",peerServer);
+
+//Data Parsing //
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+
 
 require("dotenv").config();
 const { auth,requiresAuth } = require('express-openid-connect');
@@ -37,18 +47,103 @@ app.use(auth(config));
  // res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
 //});
 
+//app.get('/',(req,res) => {
+////    res.render('home');
+//})
 
-
-
-
+const history = [];
+app.get("/profile", requiresAuth(),(req,res) => {
+  res.send(JSON.stringify(req.oidc.user));
+ 
+})
 app.get('/', requiresAuth(),(req,res) => {
  res.redirect(`/${uuidv4()}`);
 });
 app.get('/:room',(req,res) => {
-    res.render("room", {roomID:req.params.room});
+  console.log(req.params.room);
+    res.render("home", {roomID:req.params.room});
 });
 
+
+app.post('/send',  function (req, res) {
+   
+
+  var mailOpts, smtpTrans;
+
+  //Setup Nodemailer transport, I chose gmail. Create an application-specific password to avoid problems.
+  smtpTrans = nodemailer.createTransport(smtpTransport({
+      service: 'gmail',
+      //  host:'smtp.gmail.com',
+      //  port:465,
+      // secure:true,
+      auth: {
+        user: 'mukesh.19je0540@ee.iitism.ac.in', // generated ethereal user
+        pass: 'Mukesh@130301'  // generated ethereal password
+      }
+  }));
+  const output = `
+  <p>You have a new contact request</p>
+  <h3>Contact Details</h3>
+  <ul>  
+    <li>Name: ${req.body.subject}</li>
+    <li>Email: ${req.body.email}</li>
+    
+  </ul>
+  <h3>Message</h3>
+  <p>${req.body.text}</p>
+`;
+  //Mail options
+  mailOpts = {
+      from: 'mukesh.19je0540@ee.iitism.ac.in', // generated sender address
+      to: req.body.email, // list of receivers
+      subject: req.body.text, // Subject line
+      text: 'Hello world?', // plain text body
+      html: output // html body
+  };
+ smtpTrans.sendMail(mailOpts,  function (error, res) {
+  try{
+    console.log('Message sent successfully!');
+   }
+   catch(error)
+     {
+        // res.send("Email could not send due to error" +error);
+        return console.log(error);
+    }
+
+});
+
+});
+
+
+
+
+   
+
+//console.log(query.sql);
+
+
+
 io.on('connection',socket => {
+   
+        /* New user connected  */
+        console.log('A user connected');
+    
+        /* draw all old updates to this user's canvas */
+        console.log('Syncing new user"s canvas from history')
+        for(let item of history)
+          socket.emit('update_canvas',item);
+    
+        /* Recieving updates from user */
+        socket.on('update_canvas',function(data){
+    
+          /* store updates */
+          history.push(data);
+    
+          /* send updates to all sockets except sender */
+          socket.broadcast.emit('update_canvas',data);
+        });
+     
+      
   
     socket.on('new-user',(username)=>{
         console.log(username);
